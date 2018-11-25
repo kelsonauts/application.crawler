79bed2d3e98f4d3417a8d6fe289f3de0ccf7e61c
Aleksandr Lopatkin <aleksandr_lopatkin@epam.com>
2018-11-25 23:47:28 +0300

    Fixed provisioning

--- infrastructure/deploy.sh
+++ infrastructure/deploy.sh
@@ -5,10 +5,10 @@ TEMPLATE_NAME="stack.yaml"
 
 if [[ `aws cloudformation describe-stacks | jq ".Stacks[] | select(.StackName == \"${STACK_NAME}\")"` ]]; then 
 	aws cloudformation update-stack --stack-name "${STACK_NAME}"  \
-		--template-body file://"${TEMPLATE_NAME}"
+		--template-body file://"${TEMPLATE_NAME}" --capabilities CAPABILITY_IAM
 	aws cloudformation wait stack-update-complete --stack-name "${STACK_NAME}"
 else
 	aws cloudformation create-stack --stack-name "${STACK_NAME}"  \
-		--template-body file://"${TEMPLATE_NAME}"
+		--template-body file://"${TEMPLATE_NAME}" --capabilities CAPABILITY_IAM
 	aws cloudformation wait stack-create-complete --stack-name "${STACK_NAME}"
 fi
