d1cd2e0eb11b312bc5b5c97fbf2ece8a0c7182d9
Aleksandr Lopatkin <aleksandr_lopatkin@epam.com>
2018-11-26 22:46:05 +0300

    Removed coordinator functionality. One instance-one message. Changed instances to t2.medium

--- coordinator.py
+++ coordinator.py
@@ -13,21 +13,22 @@ if __name__ == "__main__":
     try:
         client = boto3.client('sqs', region_name='us-east-1')
         sqsUrl = client.get_queue_url(QueueName = "infrastructure-crawler-sqs-useast1.fifo")['QueueUrl']
-        while(True):
-            response = client.receive_message(QueueUrl=sqsUrl, MaxNumberOfMessages=1)
-            messages = response.get('Messages')
-            if (messages == None):
-                break
-            msg = messages[0]
-            body = json.loads(msg['Body'])
-            receiptHandle = msg['ReceiptHandle']
-            start = body['Start']
-            end = body['End']
-            crawler = worker.Worker(start, end)
-            crawler.run()
-            crawler = None
-            os.system("aws s3 cp /data s3://infrastructure-storages-useast1-s3bucket/data/ --recursive")
-            client.delete_message(QueueUrl = sqsUrl, ReceiptHandle = receiptHandle)
+
+        response = client.receive_message(QueueUrl=sqsUrl, MaxNumberOfMessages=1)
+        messages = response.get('Messages')
+        if (messages == None):
+            logging.info("Nothing to process")
+            raise Exception("Aborting coordinator because nothing to process")
+        msg = messages[0]
+        body = json.loads(msg['Body'])
+        receiptHandle = msg['ReceiptHandle']
+        start = body['Start']
+        end = body['End']
+        crawler = worker.Worker(start, end)
+        crawler.run()
+        crawler = None
+        os.system("aws s3 cp /data s3://infrastructure-storages-useast1-s3bucket/data/ --recursive")
+        client.delete_message(QueueUrl = sqsUrl, ReceiptHandle = receiptHandle)
     except Exception:
         logging.error("Got an exception")
         logging.error(traceback.print_exc())
