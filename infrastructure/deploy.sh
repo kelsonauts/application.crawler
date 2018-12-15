207c7bde1dec167f58d058fbad2a5fcd27953e09
Aleksandr Lopatkin <aleksandr_lopatkin@epam.com>
2018-12-15 15:22:31 +0300

    Add region to deployment script

--- infrastructure/deploy.sh
+++ infrastructure/deploy.sh
@@ -7,11 +7,11 @@ STACK_NAME="crawler"
 TEMPLATE_NAME="stack.yaml"
 
 if [[ `aws cloudformation describe-stacks | jq ".Stacks[] | select(.StackName == \"${STACK_NAME}\")"` ]]; then 
-	aws cloudformation update-stack --stack-name "${STACK_NAME}"  \
+	aws cloudformation update-stack --stack-name "${STACK_NAME}" --region us-east-1  \
 		--template-body file://"${TEMPLATE_NAME}" --capabilities CAPABILITY_IAM
-	aws cloudformation wait stack-update-complete --stack-name "${STACK_NAME}"
+	aws cloudformation wait stack-update-complete --stack-name "${STACK_NAME}" --region us-east-1
 else
-	aws cloudformation create-stack --stack-name "${STACK_NAME}"  \
+	aws cloudformation create-stack --stack-name "${STACK_NAME}" --region us-east-1  \
 		--template-body file://"${TEMPLATE_NAME}" --capabilities CAPABILITY_IAM
-	aws cloudformation wait stack-create-complete --stack-name "${STACK_NAME}"
+	aws cloudformation wait stack-create-complete --stack-name "${STACK_NAME}" --region us-east-1
 fi
