f9ccc5e7d07932466f536305d4306dccd03bfe7c
Aleksandr Lopatkin <aleksandr_lopatkin@epam.com>
2018-12-15 17:45:28 +0300

    Fixed deployment script

--- infrastructure/deploy.sh
+++ infrastructure/deploy.sh
@@ -6,7 +6,7 @@ cd $(dirname $0)
 STACK_NAME="crawler"
 TEMPLATE_NAME="stack.yaml"
 
-if [[ `aws cloudformation describe-stacks | jq ".Stacks[] | select(.StackName == \"${STACK_NAME}\")"` ]]; then 
+if [[ `aws cloudformation describe-stacks --region us-east-1 | jq ".Stacks[] | select(.StackName == \"${STACK_NAME}\")"` ]]; then 
 	aws cloudformation update-stack --stack-name "${STACK_NAME}" --region us-east-1  \
 		--template-body file://"${TEMPLATE_NAME}" --capabilities CAPABILITY_IAM
 	aws cloudformation wait stack-update-complete --stack-name "${STACK_NAME}" --region us-east-1
